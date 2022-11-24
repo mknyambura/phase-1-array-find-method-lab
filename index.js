f5a7163fb176ef0d871c62f05330fa6b2c071c51
mknyambura <kariuki.mercie@gmail.com>
2022-11-24 13:36:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "w"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+];
+function superbowlWin(collection){
+   for (const game of collection){
+    if (game.result === "W"){
+        return game.year
+    }
+   }
+};
